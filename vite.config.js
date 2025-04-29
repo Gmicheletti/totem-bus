971f2addcf45a94ccfa9405e9dccabe03923b2bf
Guilherme Micheletti <guilhermemicheletti@MacBook-Pro-de-Guilherme.local>
2025-04-28 22:57:33 -0300

    atualizacao http

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 export default defineConfig({
-  plugins: [react()], // <-- isso precisa estar ativo
+  plugins: [react()],
   server: {
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://mobile-l.sitbus.com.br:6060',
